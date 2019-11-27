df3ee79cc250bc01066e4795d47ed04d36eeb1a5
RaviC Rachamalla <rach0022@algonquinlive.com>
2019-11-27 16:42:16 -0500

    moved steve starter code to .old files, set up a basic html page that takes user input and displays it on the page

--- js/main.js
+++ js/main.js
@@ -1,50 +1,27 @@
-const APP = {
-    active: "green",
-    pages: [],
+const app = {
+    searchQuery: null,
+    actorID: null,
+    movieID: null,
     baseURL: null,
+    pages: [],
+    active: null,
     init: () => {
-        APP.pages = document.querySelectorAll(".page");
-        let links = document.querySelectorAll("[data-href]");
-        links.forEach(link => {
-            link.addEventListener("click", APP.nav);
-        });
-        //get the base url to use in the app
-        APP.baseURL = location.href.split("#")[0];
-        let hash = location.hash;
-        //check for current url hash
-        if (hash && hash != "#") {
-            //there is an id in the url
-            APP.active = hash.replace("#", "");
-            APP.showPage(APP.active);
-        } else {
-            //no url so use our default
-            history.replaceState({}, APP.active, `${APP.baseURL}#${APP.active}`);
-            APP.showPage(APP.active);
-        }
-        //handle the back button
-        window.addEventListener("popstate", APP.poppy);
-},
-    nav: ev => {
+        console.log("the script is loaded");
+        document.getElementById('btn').addEventListener('click',app.search);
+    },
+    search: ev => {
+        //stop the form being submitted
         ev.preventDefault();
-        ev.stopPropagation();
-        let link = ev.target;
-        let target = link.getAttribute("data-href");
-        //update URL
-        history.pushState({}, target, `${APP.baseURL}#${target}`);
-        //change the display of the "page"
-        APP.showPage(target);
-        //use switch case with target for page specific things
-    },
-    showPage: target => {
-        document.querySelector(".active").classList.remove("active");
-        document.querySelector(`#${target}`).classList.add("active");
-    },
-    poppy: ev => {
-        //get the id
-        let target = location.hash.replace("#", "");
-        APP.showPage(target);
+
+        app.searchQuery = document.getElementById('actor').value;
+        console.log(app.searchQuery);
+        let p = document.createElement('p');
+        p.textContent = app.searchQuery;
+        document.querySelector('main').appendChild(p);
+
+        //old way to reset form
+        // document.forms[0].reset();
+        document.querySelector('form').reset();
     }
-};
-  
-document.addEventListener("DOMContentLoaded", APP.init);
-  
+}
+document.addEventListener('DOMContentLoaded', app.init);
