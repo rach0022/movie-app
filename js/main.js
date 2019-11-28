61aa13c329a2a560eabbe0504b676a70c520809a
RaviC Rachamalla <rach0022@algonquinlive.com>
2019-11-28 15:54:07 -0500

    got test movie building working for movies and cast id, now switching movie selection to whomever the user clicks on

--- js/main.js
+++ js/main.js
@@ -13,6 +13,59 @@ const app = {
         console.log("the script is loaded");
         document.getElementById('btn').addEventListener('click',app.search);
     },
+
+    //build a function to process the movie data from the double fetch promise all call
+    processMovies: moviePromise => {
+        moviePromise.then(data => {
+
+            //cehck if it is cast data or movie data
+            if(data.cast){
+
+                //if it is cast data loop through each cast member and build a name
+                data.cast.forEach(member => {
+                    let p = document.createElement('p');
+                    p.textContent = `Actor: ${member.name} | Character in movie: ${member.character}`;
+                    document.querySelector('main').appendChild(p);
+                })
+                console.log("cast data:", data.cast);
+                
+
+                // data.cast.forea 
+            } else {
+                console.log("movie data", data);
+                let p = document.createElement("p");
+                p.textContent = data.tagline + data.release_date;
+                document.querySelector('main').appendChild(p);
+            }
+            
+        })
+    },
+
+    //call this function when you need to build teh movie page and also cast details for that movie
+    buildMoviePage: ev => {
+
+        //now to see if i can handle the next fetch call
+        //movie url is the
+        app.movieID = ev.target.getAttribute("data-movieid");
+        let movieUrl = fetch(`https://api.themoviedb.org/3/movie/${app.movieID}?api_key=${app.apiKey}`);
+        let castURL = fetch(`https://api.themoviedb.org/3/movie/${app.movieID}/credits?api_key=${app.apiKey}`);
+
+        let requests = [movieUrl, castURL];
+
+        //now to do the promise for both urls:
+
+        Promise.all(requests)
+            .then(movies => {
+                
+                //taken from steve promise all fetch video
+                movies.forEach(movie => {
+                    app.processMovies(movie.json());
+                })
+            })
+            .catch(err => {
+                console.log(err.message);
+            })
+    },
     search: ev => {
         //stop the form being submitted
         ev.preventDefault();
@@ -28,60 +81,43 @@ const app = {
         // document.forms[0].reset();
         document.querySelector('form').reset();
 
+        //if there is a search query
+        if(app.searchQuery){
 
-        //how to get a /search/person
-        let url = `https://api.themoviedb.org/3/search/person?api_key=${app.apiKey}&query=${app.searchQuery}`;
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
+            //how to get a /search/person
+            let url = `https://api.themoviedb.org/3/search/person?api_key=${app.apiKey}&query=${app.searchQuery}`;
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
 
-                //how to display all the actors with a name match
-                data.results.forEach(actor => {
-                    let d = document.createElement('p');
-                    d.textContent = actor.name;
-                    document.querySelector('main').appendChild(d);
+                    //how to display all the actors with a name match
+                    data.results.forEach(actor => {
+                        let d = document.createElement('p');
+                        d.textContent = actor.name;
+                        document.querySelector('main').appendChild(d);
+                    })
+
+                    //now to display the movie results for the first actor returned (for testing)
+                    //first check if data.results is not null
+                    
+                    if(data.results[0]){
+                        data.results[0].known_for.forEach(movie => {
+                            //create the element holding the movie results
+                            let testMovies = document.createElement('p');
+                            testMovies.textContent = movie.id + " " + movie.original_title;
+                            testMovies.setAttribute("data-movieid", movie.id);
+
+                            //add a click listener to test if movies can generate movie & cast data
+                            testMovies.addEventListener('click', app.buildMoviePage);
+                            document.querySelector('main').appendChild(testMovies);
+                        })
+                        app.movieID = data.results[0].known_for[0].id;
+                    }   
+                    })
+                .catch(err => {
+                    console.log(err.message);
                 })
-
-                //now to display the movie results for the first actor returned (for testing)
-                //first check if data.results is not null
-                
-                if(data.results[0]){
-                    data.results[0].known_for.forEach(movie => {
-                        //create the element holding the movie results
-                        let testMovies = document.createElement('p');
-                        testMovies.textContent = movie.id + " " + movie.original_title;
-                        testMovies.setAttribute("data-movieid", movie.id);
-                        document.querySelector('main').appendChild(testMovies);
-                    })
-                    app.movieID = data.results[0].known_for[0].id;
-                }
-
-                //now to see if i can handle the next fetch call
-                //movie url is the 
-                let movieUrl = `https://api.themoviedb.org/3/movie/${app.movieID}?api_key=${app.apiKey}`
-                let castURL = `https://api.themoviedb.org/3/movie/${app.movieID}/credits?api_key=${app.apiKey}`
-
-                let requests = [fetch(movieUrl), fetch(castURL)];
-
-                //now to do the promise for both urls:
-
-                Promise.all(requests)
-                    .then(responses => {
-                        return [responses[0].json(), responses[1].json()];
-                    })
-                    .then(details => {
-                        console.log(details[0]);
-                    })
-                    .catch(err => {
-                        console.log(err.message);
-                    })
-
-                        
-                        
-                    })
-                    .catch(err => {
-                        console.log(err.message);
-                    })
+            }
     }
 }
 document.addEventListener('DOMContentLoaded', app.init);
